cf09d4a9dbc29393c4eed38ee28172f248a5b0c6
Florian Isopp <florian.johannes.isopp@gmail.com>
2022-07-26 14:53:35 +0200

    base scaff

---
+++ DeviceMapper/IDeviceMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceMapper
+{
+    public interface IDeviceMapper
+    {
+        void Initialize();
+
+        IList<IDeviceConfiguration> FindDevices();
+
+        IList<IDeviceConfiguration> FindDevicesFromConfig();
+
+        IDeviceConfiguration GetConfiguration(DeviceMapperProtocol protocol, string config, Type t);
+
+    }
+}
